2da3464278112d346f60dd12fa28a35d62014e61
0xshiba1 <158560741+0xshiba1@users.noreply.github.com>
2024-04-19 10:48:41 +0800

    add polyfill, update tsconfig

---
+++ frontend/src/lib/abortSignalPolyfill.ts
@@ -0,0 +1,7 @@
+if (!AbortSignal.timeout) {
+  AbortSignal.timeout = (ms) => {
+    const controller = new AbortController();
+    setTimeout(() => controller.abort(new DOMException("TimeoutError")), ms);
+    return controller.signal;
+  };
+}
