8c541464191f0cdb13199784c2d931b03990c3ea
0xshiba1 <158560741+0xshiba1@users.noreply.github.com>
2024-04-30 12:01:01 +0800

    fetch compressed obligations

--- frontend/src/app/api/obligations/route.ts
+++ frontend/src/app/api/obligations/route.ts
@@ -1,11 +1,16 @@
+import { inflateSync } from "zlib";
+
 import { NextRequest, NextResponse } from "next/server";
 
 const API_URL = "https://api.suilend.fi";
 
 export async function GET(request: NextRequest) {
   try {
-    const response = await fetch(`${API_URL}/obligations/all`);
-    const json = await response.json();
+    const response = await fetch(`${API_URL}/obligations/all?compressed=true`);
+    const compressedJson = await response.json();
+    const json = JSON.parse(
+      inflateSync(Buffer.from(compressedJson.data, "base64")).toString(),
+    );
 
     return NextResponse.json(json, { status: 200 });
   } catch (err) {
