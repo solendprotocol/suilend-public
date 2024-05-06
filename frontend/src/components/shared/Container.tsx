609047b5ba203b3ef5c0cadb2014fe36de8322d7
0xshiba1 <158560741+0xshiba1@users.noreply.github.com>
2024-05-06 21:12:08 +0800

    add container

--- frontend/src/components/shared/Container.tsx
+++ frontend/src/components/shared/Container.tsx
@@ -1,8 +1,21 @@
 import { PropsWithChildren } from "react";
 
-export default function Container({ children }: PropsWithChildren) {
+import { ClassValue } from "clsx";
+
+import { cn } from "@/lib/utils";
+
+interface ContainerProps extends PropsWithChildren {
+  className?: ClassValue;
+}
+
+export default function Container({ className, children }: ContainerProps) {
   return (
-    <div className="mx-auto flex w-full max-w-[1440px] flex-col items-center px-4 md:px-10">
+    <div
+      className={cn(
+        "mx-auto flex w-full max-w-[1440px] flex-col items-center px-4 md:px-10",
+        className,
+      )}
+    >
       {children}
     </div>
   );
