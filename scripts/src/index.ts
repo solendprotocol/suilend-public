7b6a5fb4abce21a623065f4b0b6446072d2128d6
0xripleys <105607696+0xripleys@users.noreply.github.com>
2024-05-14 22:43:17 -0400

    contract upgrade + crank script changes (#49)
    * contract upgrade
    
    * v1.0.6
    
    ---------
    
    Co-authored-by: 0xshiba1 <158560741+0xshiba1@users.noreply.github.com>

--- scripts/src/index.ts
+++ scripts/src/index.ts
@@ -14,9 +14,8 @@ import { TransactionBlock } from "@mysten/sui.js/transactions";
 import { Side } from "../../sdk/src/core/types";
 import { fromB64, fromHEX } from "@mysten/sui.js/utils";
 import { Ed25519Keypair } from "@mysten/sui.js/keypairs/ed25519";
+import { Obligation } from "../../sdk/src/mainnet/_generated/suilend/obligation/structs";
 
-const REWARD_RESERVE_INDEX = 0;
-const REWARD_INDEX = 0;
 const REWARD_TYPE = "0x2::sui::SUI";
 const DEPOSIT_RESERVE_INDEX = 0;
 const IS_DEPOSIT_REWARD = true;
@@ -24,48 +23,52 @@ const IS_DEPOSIT_REWARD = true;
 const BATCH_SIZE = 50;
 
 const keypair = Ed25519Keypair.fromSecretKey(
-  fromB64(process.env.SUI_SECRET_KEY!),
+  fromB64(process.env.SUI_SECRET_KEY!)
 );
 
-async function main() {
-  console.log("hi");
-  const client = new SuiClient({
-    url: "https://fullnode.mainnet.sui.io/",
-  });
+async function crankRewards(
+  client: SuiClient,
+  obligations: Obligation<string>[],
+  rewardReserveIndex: number,
+  rewardIndex: number
+) {
   const suilendClient = await SuilendClient.initialize(
     LENDING_MARKET_ID,
     LENDING_MARKET_TYPE,
-    client,
+    client
   );
   const connection = new SuiPriceServiceConnection(
-    "https://hermes.pyth.network",
+    "https://hermes.pyth.network"
   );
-
-  const obligations = await fetchAllObligationsForMarket(
-    client,
-    LENDING_MARKET_ID,
-  );
-  console.log("Fetched obligations");
 
   let lendingMarket = await LendingMarket.fetch(
     client,
     phantom(LENDING_MARKET_TYPE),
-    LENDING_MARKET_ID,
+    LENDING_MARKET_ID
   );
 
   const now = Math.floor(Date.now() / 1000);
   let refreshedReserves = lendingMarket.reserves as Reserve<string>[];
   refreshedReserves = await simulate.refreshReservePrice(
     lendingMarket.reserves.map((r) => simulate.compoundReserveInterest(r, now)),
-    connection,
+    connection
   );
 
   const poolManager = IS_DEPOSIT_REWARD
-    ? refreshedReserves[REWARD_RESERVE_INDEX].depositsPoolRewardManager
-    : refreshedReserves[REWARD_RESERVE_INDEX].borrowsPoolRewardManager;
+    ? refreshedReserves[rewardReserveIndex].depositsPoolRewardManager
+    : refreshedReserves[rewardReserveIndex].borrowsPoolRewardManager;
 
   if (!poolManager) {
     throw new Error("Pool manager not found");
+  }
+
+  const poolReward = poolManager?.poolRewards[rewardIndex];
+  const endTimeMs = poolReward?.endTimeMs;
+
+  // console.log("Pool reward: ", poolReward);
+  if (Date.now() < endTimeMs) {
+    console.log("Pool reward not ready");
+    return;
   }
 
   const refreshedObligations = obligations
@@ -74,20 +77,20 @@ async function main() {
     })
     .filter((obligation) => {
       let userRewardManager = obligation.userRewardManagers.find(
-        (u) => u.poolRewardManagerId == poolManager?.id,
+        (u) => u.poolRewardManagerId == poolManager?.id
       );
 
       if (!userRewardManager) {
         return false;
       }
 
-      let userReward = userRewardManager.rewards[REWARD_INDEX];
+      let userReward = userRewardManager.rewards[rewardIndex];
       return userReward != null;
     });
 
   console.log("Crankable obligations length: ", refreshedObligations.length);
 
-  console.log(poolManager.id);
+  // console.log(poolManager.id);
   console.log(poolManager);
 
   let i = 0;
@@ -98,12 +101,12 @@ async function main() {
 
       suilendClient.claimRewardsAndDeposit(
         obligation.id,
-        BigInt(REWARD_RESERVE_INDEX),
-        BigInt(REWARD_INDEX),
+        BigInt(rewardReserveIndex),
+        BigInt(rewardIndex),
         REWARD_TYPE,
         IS_DEPOSIT_REWARD ? Side.DEPOSIT : Side.BORROW,
         BigInt(DEPOSIT_RESERVE_INDEX),
-        txb,
+        txb
       );
 
       i++;
@@ -114,9 +117,46 @@ async function main() {
       signer: keypair,
     });
     console.log(res);
-  }
 
-  console.log(lendingMarket);
+    // sleep for 50ms
+    await new Promise((resolve) => setTimeout(resolve, 50));
+  }
+}
+
+async function main() {
+  const client = new SuiClient({
+    url: `https://solendf-suishar-0c55.mainnet.sui.rpcpool.com/${
+      process.env.NEXT_PUBLIC_SUI_TRITON_ONE_DEV_API_KEY ?? ""
+    }`
+  });
+
+  const obligations = await fetchAllObligationsForMarket(
+    client,
+    LENDING_MARKET_ID
+  );
+  console.log("Fetched obligations");
+
+  for (let rewardIndex = 0; rewardIndex < 5; rewardIndex++) {
+    for (
+      let rewardReserveIndex = 0;
+      rewardReserveIndex < 3;
+      rewardReserveIndex++
+    ) {
+      try {
+        console.log(
+          `Cranking rewards for reserve ${rewardReserveIndex} and reward ${rewardIndex}`
+        );
+        await crankRewards(
+          client,
+          obligations,
+          rewardReserveIndex,
+          rewardIndex
+        );
+      } catch (e) {
+        console.error(e);
+      }
+    }
+  }
 }
 
 main();
--- sdk/src/mainnet/_generated/suilend/index.ts
+++ sdk/src/mainnet/_generated/suilend/index.ts
@@ -1,4 +1,4 @@
 export const PACKAGE_ID =
   "0xf95b06141ed4a174f239417323bde3f209b972f5930d8521ea38a52aff3a6ddf";
 export const PUBLISHED_AT =
-  "0x5fbbb0297c80ae2c382bbe21e71408b3a11259b46cdbaaa1bb261c1d5fc93f02";
+  "0x157dbf1830dab58ec67e025c1200a32a22a31150beba4d3de8e39571547f8cc2";
