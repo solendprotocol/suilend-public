2f1c5b309add06be3fe284547161f556aecf1516
Samuel Oloruntoba <5002506+kayandra@users.noreply.github.com>
2024-05-20 10:31:51 +0100

    Update SuiPriceServiceConnection.ts
    Typescript cannot identify a constructor on `SuiPriceServiceConnection` even though it extends `PriceServiceConnection`, so a permanent fix on this is defining one and making a super call.

--- pyth-sdk/src/SuiPriceServiceConnection.ts
+++ pyth-sdk/src/SuiPriceServiceConnection.ts
@@ -1,10 +1,21 @@
 import {
   PriceServiceConnection,
   HexString,
+  type PriceServiceConnectionConfig,
 } from "@pythnetwork/price-service-client";
 import { Buffer } from "buffer";
 
 export class SuiPriceServiceConnection extends PriceServiceConnection {
+  /**
+   * Constructs a new Connection.
+   *
+   * @param endpoint endpoint URL to the price service. Example: https://website/example/
+   * @param config Optional PriceServiceConnectionConfig for custom configurations.
+   */
+  constructor(endpoint: string, config?: PriceServiceConnectionConfig) {
+    super(endpoint, config);
+  }
+  
   /**
    * Gets price update data (either batch price attestation VAAs or accumulator messages, depending on the chosen endpoint), which then
    * can be submitted to the Pyth contract to update the prices. This will throw an axios error if there is a network problem or
