ff4eed9a808ddd78477d869fe7e9722140ba7ff0
0xshiba1 <158560741+0xshiba1@users.noreply.github.com>
2024-05-22 18:48:59 +0800

    events, simulate

--- sdk/src/core/parsers/apiReserveAssetDataEvent.ts
+++ sdk/src/core/parsers/apiReserveAssetDataEvent.ts
@@ -13,100 +13,67 @@ export type ParsedReserveAssetDataEvent = ReturnType<
   typeof parseReserveAssetDataEvent
 >;
 
-const calculateBorrowAprPercent = (
-  utilizationPercent: BigNumber,
-  reserve: ParsedReserve,
-) => {
-  const config = reserve.config;
-
-  let i = 1;
-  while (i < config.interestRate.length) {
-    const leftUtilPercent = config.interestRate[i - 1].utilPercent;
-    const leftAprPercent = config.interestRate[i - 1].aprPercent;
-
-    const rightUtilPercent = config.interestRate[i].utilPercent;
-    const rightAprPercent = config.interestRate[i].aprPercent;
-
-    if (
-      utilizationPercent.gte(leftUtilPercent) &&
-      utilizationPercent.lte(rightUtilPercent)
-    ) {
-      const weight = new BigNumber(
-        utilizationPercent.minus(leftUtilPercent),
-      ).div(rightUtilPercent.minus(leftUtilPercent));
-
-      return leftAprPercent.plus(
-        weight.times(rightAprPercent.minus(leftAprPercent)),
-      );
-    }
-    i = i + 1;
-  }
-  // Should never reach here
-  return new BigNumber(0);
-};
-
 export const parseReserveAssetDataEvent = (
   event: ApiReserveAssetDataEvent,
   reserve: ParsedReserve,
 ) => {
+  const availableAmount = new BigNumber(event.availableAmount)
+    .div(WAD)
+    .div(10 ** reserve.mintDecimals);
   const depositedAmount = new BigNumber(event.supplyAmount)
     .div(WAD)
     .div(10 ** reserve.mintDecimals);
   const borrowedAmount = new BigNumber(event.borrowedAmount)
     .div(WAD)
     .div(10 ** reserve.mintDecimals);
-  const availableAmount = new BigNumber(event.availableAmount)
-    .div(WAD)
-    .div(10 ** reserve.mintDecimals);
+
+  const availableAmountUsd = new BigNumber(
+    event.availableAmountUsdEstimate,
+  ).div(WAD);
+  const depositedAmountUsd = new BigNumber(event.supplyAmountUsdEstimate).div(
+    WAD,
+  );
+  const borrowedAmountUsd = new BigNumber(event.borrowedAmountUsdEstimate).div(
+    WAD,
+  );
+
+  const borrowAprPercent = new BigNumber(event.borrowApr).div(WAD).times(100);
+  const depositAprPercent = new BigNumber(event.supplyApr).div(WAD).times(100);
 
   const ctokenSupply = new BigNumber(event.ctokenSupply).div(
     10 ** reserve.mintDecimals,
   );
-
-  const utilizationPercent = depositedAmount.eq(0)
-    ? new BigNumber(0)
-    : borrowedAmount.div(depositedAmount).times(100);
-
-  const borrowAprPercent = calculateBorrowAprPercent(
-    utilizationPercent,
-    reserve,
+  const cumulativeBorrowRate = new BigNumber(event.cumulativeBorrowRate).div(
+    WAD,
   );
-  const depositAprPercent = utilizationPercent
-    .div(100)
-    .times(borrowAprPercent.div(100))
-    .times(
-      new BigNumber(100).minus(
-        new BigNumber(reserve.config.spreadFeeBps).div(100),
-      ),
-    );
 
   const price = new BigNumber(event.price).div(WAD);
   const smoothedPrice = new BigNumber(event.smoothedPrice).div(WAD);
   const minPrice = BigNumber.min(price, smoothedPrice);
   const maxPrice = BigNumber.max(price, smoothedPrice);
 
-  const depositedAmountUsd = depositedAmount.times(price);
-  const borrowedAmountUsd = borrowedAmount.times(price);
-  const availableAmountUsd = availableAmount.times(price);
+  // Custom
+  const utilizationPercent = depositedAmount.eq(0)
+    ? new BigNumber(0)
+    : borrowedAmount.div(depositedAmount).times(100);
 
   return {
     id: event.id,
-    lendingMarket: event.lendingMarketId,
+    lendingMarketId: event.lendingMarketId,
     coinType: normalizeStructTag(event.coinType),
     reserveId: event.reserveId,
+    availableAmount,
     depositedAmount,
     borrowedAmount,
-    availableAmount,
+    availableAmountUsd,
     depositedAmountUsd,
     borrowedAmountUsd,
-    availableAmountUsd,
-    utilizationPercent: utilizationPercent,
-    borrowAprPercent: borrowAprPercent,
-    depositAprPercent: depositAprPercent,
-    ctokenSupply: ctokenSupply,
-    cumulativeBorrowRate: event.cumulativeBorrowRate,
-    price: price,
-    smoothedPrice: smoothedPrice,
+    borrowAprPercent,
+    depositAprPercent,
+    ctokenSupply,
+    cumulativeBorrowRate,
+    price,
+    smoothedPrice,
     minPrice,
     maxPrice,
     priceLastUpdateTimestampS: event.priceLastUpdateTimestampS,
@@ -114,6 +81,8 @@ export const parseReserveAssetDataEvent = (
     digest: event.digest,
     eventIndex: event.eventIndex,
     sender: event.sender,
+
+    utilizationPercent,
   };
 };
 
