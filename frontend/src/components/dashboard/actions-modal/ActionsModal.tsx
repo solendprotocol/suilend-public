533178a6cc91ec425f0eeae8ac4c69c654fc9f08
0xshiba1 <158560741+0xshiba1@users.noreply.github.com>
2024-06-25 15:22:33 +0800

    refactor actions defs

--- frontend/src/components/dashboard/actions-modal/ActionsModal.tsx
+++ frontend/src/components/dashboard/actions-modal/ActionsModal.tsx
@@ -14,12 +14,11 @@ import ParametersPanel from "@/components/dashboard/actions-modal/ParametersPane
 import Tabs from "@/components/shared/Tabs";
 import { AppData, useAppContext } from "@/contexts/AppContext";
 import useBreakpoint from "@/hooks/useBreakpoint";
-import { isSui } from "@/lib/coinType";
 import {
-  SUI_DEPOSIT_GAS_MIN,
-  SUI_REPAY_GAS_MIN,
-  msPerYear,
-} from "@/lib/constants";
+  getMaxValue,
+  getSubmitButtonNoValueState,
+  getSubmitButtonState,
+} from "@/lib/actions";
 import { Action } from "@/lib/types";
 
 export default function ActionsModal() {
@@ -61,68 +60,6 @@ export default function ActionsModal() {
     if (reserve === undefined) return undefined;
 
     if (selectedTab === Tab.DEPOSIT) {
-      const maxCalculations = (() => {
-        const coinBalanceForReserve =
-          data.coinBalancesMap[reserve.coinType]?.balance ?? new BigNumber(0);
-
-        // Calculate safe deposit limit (subtract 10 mins of deposit APR from cap)
-        const tenMinsDepositAprPercent = reserve.depositAprPercent
-          .div(msPerYear)
-          .times(10 * 60 * 1000);
-
-        const safeDepositLimit = reserve.config.depositLimit.minus(
-          reserve.depositedAmount.times(tenMinsDepositAprPercent.div(100)),
-        );
-        const safeDepositLimitUsd = reserve.config.depositLimitUsd.minus(
-          reserve.depositedAmount
-            .times(reserve.maxPrice)
-            .times(tenMinsDepositAprPercent.div(100)),
-        );
-
-        const result = [
-          {
-            reason: "Insufficient balance",
-            isDisabled: true,
-            value: coinBalanceForReserve,
-          },
-          {
-            reason: "Exceeds reserve deposit limit",
-            isDisabled: true,
-            value: BigNumber.max(
-              safeDepositLimit.minus(reserve.depositedAmount),
-              0,
-            ),
-          },
-          {
-            reason: "Exceeds reserve USD deposit limit",
-            isDisabled: true,
-            value: BigNumber.max(
-              safeDepositLimitUsd
-                .minus(reserve.depositedAmount.times(reserve.maxPrice))
-                .div(reserve.maxPrice),
-              0,
-            ),
-          },
-        ];
-        if (isSui(reserve.coinType))
-          result.push({
-            reason: `${SUI_DEPOSIT_GAS_MIN} SUI should be saved for gas`,
-            isDisabled: true,
-            value: coinBalanceForReserve.minus(SUI_DEPOSIT_GAS_MIN),
-          });
-
-        return result;
-      })();
-
-      const getMaxValue = () => {
-        return BigNumber.max(
-          new BigNumber(0),
-          BigNumber.min(
-            ...Object.values(maxCalculations).map((calc) => calc.value),
-          ),
-        ).toFixed(reserve.mintDecimals, BigNumber.ROUND_DOWN);
-      };
-
       const getNewCalculations = (value: string) => {
         if (!value.length)
           return {
@@ -154,112 +91,25 @@ export default function ActionsModal() {
         };
       };
 
-      const getSubmitButtonNoValueState = () => {
-        const borrowPosition = obligation?.borrows?.find(
-          (d) => d.coinType === reserve.coinType,
-        );
-        const borrowedAmount =
-          borrowPosition?.borrowedAmount ?? new BigNumber(0);
-
-        if (reserve.depositedAmount.gte(reserve.config.depositLimit))
-          return {
-            isDisabled: true,
-            title: "Reserve deposit limit reached",
-          };
-        if (
-          new BigNumber(reserve.depositedAmountUsd).gte(
-            reserve.config.depositLimitUsd,
-          )
-        )
-          return {
-            isDisabled: true,
-            title: "Reserve USD deposit limit reached",
-          };
-        if (borrowedAmount.gt(0.01))
-          return { isDisabled: true, title: "Cannot deposit borrowed asset" };
-        return undefined;
-      };
-
-      const getSubmitButtonState = (value: string) => {
-        for (const calc of maxCalculations) {
-          if (new BigNumber(value).gt(calc.value))
-            return { isDisabled: calc.isDisabled, title: calc.reason };
-        }
-        return undefined;
-      };
-
       return {
         action: Action.DEPOSIT,
         actionPastTense: "deposited",
-        getMaxValue,
+        getMaxValue: getMaxValue(Action.DEPOSIT, reserve, data, obligation),
         getNewCalculations,
-        getSubmitButtonNoValueState,
-        getSubmitButtonState,
+        getSubmitButtonNoValueState: getSubmitButtonNoValueState(
+          Action.DEPOSIT,
+          reserve,
+          obligation,
+        ),
+        getSubmitButtonState: getSubmitButtonState(
+          Action.DEPOSIT,
+          reserve,
+          data,
+          obligation,
+        ),
         submit: deposit,
       };
     } else if (selectedTab === Tab.BORROW) {
-      const MIN_AVAILABLE_AMOUNT = 100;
-
-      const maxCalculations = (() => {
-        const borrowFee = reserve.config.borrowFeeBps / 10000;
-
-        return [
-          {
-            reason: "Insufficient liquidity to borrow",
-            isDisabled: true,
-            value: reserve.availableAmount
-              .minus(
-                new BigNumber(MIN_AVAILABLE_AMOUNT).div(
-                  10 ** reserve.mintDecimals,
-                ),
-              )
-              .div(1 + borrowFee),
-          },
-          {
-            reason: "Over reserve borrow limit",
-            isDisabled: true,
-            value: reserve.config.borrowLimit
-              .minus(reserve.borrowedAmount)
-              .div(1 + borrowFee),
-          },
-          {
-            reason: "Borrows cannot exceed borrow limit",
-            isDisabled: true,
-            value:
-              !obligation ||
-              obligation.maxPriceWeightedBorrowsUsd.gt(
-                obligation.minPriceBorrowLimitUsd,
-              )
-                ? new BigNumber(0)
-                : obligation.minPriceBorrowLimitUsd
-                    .minus(obligation.maxPriceWeightedBorrowsUsd)
-                    .div(
-                      reserve.maxPrice.times(
-                        reserve.config.borrowWeightBps / 10000,
-                      ),
-                    )
-                    .div(1 + borrowFee),
-          },
-          {
-            reason: "Pool outflow rate limit surpassed",
-            isDisabled: true,
-            value: data.lendingMarket.rateLimiter.remainingOutflow
-              .div(reserve.maxPrice)
-              .div(reserve.config.borrowWeightBps / 10000)
-              .div(1 + borrowFee),
-          },
-        ];
-      })();
-
-      const getMaxValue = () => {
-        return BigNumber.max(
-          new BigNumber(0),
-          BigNumber.min(
-            ...Object.values(maxCalculations).map((calc) => calc.value),
-          ),
-        ).toFixed(reserve.mintDecimals, BigNumber.ROUND_DOWN);
-      };
-
       const getNewCalculations = (value: string) => {
         if (!value.length)
           return {
@@ -292,106 +142,25 @@ export default function ActionsModal() {
         };
       };
 
-      const getSubmitButtonNoValueState = () => {
-        const depositPosition = obligation?.deposits?.find(
-          (d) => d.coinType === reserve.coinType,
-        );
-        const depositedAmount =
-          depositPosition?.depositedAmount ?? new BigNumber(0);
-
-        if (reserve.borrowedAmount.gte(reserve.config.borrowLimit))
-          return {
-            isDisabled: true,
-            title: "Reserve borrow limit reached",
-          };
-        if (
-          new BigNumber(reserve.borrowedAmount.times(reserve.price)).gte(
-            reserve.config.borrowLimitUsd,
-          )
-        )
-          return {
-            isDisabled: true,
-            title: "Reserve USD borrow limit reached",
-          };
-        if (depositedAmount.gt(0.01))
-          return { isDisabled: true, title: "Cannot borrow deposited asset" };
-        return undefined;
-      };
-
-      const getSubmitButtonState = (value: string) => {
-        for (const calc of maxCalculations) {
-          if (new BigNumber(value).gt(calc.value))
-            return { isDisabled: calc.isDisabled, title: calc.reason };
-        }
-        return undefined;
-      };
-
       return {
         action: Action.BORROW,
         actionPastTense: "borrowed",
-        getMaxValue,
+        getMaxValue: getMaxValue(Action.BORROW, reserve, data, obligation),
         getNewCalculations,
-        getSubmitButtonNoValueState,
-        getSubmitButtonState,
+        getSubmitButtonNoValueState: getSubmitButtonNoValueState(
+          Action.BORROW,
+          reserve,
+          obligation,
+        ),
+        getSubmitButtonState: getSubmitButtonState(
+          Action.BORROW,
+          reserve,
+          data,
+          obligation,
+        ),
         submit: borrow,
       };
     } else if (selectedTab === Tab.WITHDRAW) {
-      const MIN_AVAILABLE_AMOUNT = 100;
-
-      const maxCalculations = (() => {
-        const position = obligation?.deposits.find(
-          (deposit) => deposit.coinType === reserve.coinType,
-        );
-        const depositedAmount = position?.depositedAmount ?? new BigNumber(0);
-
-        return [
-          {
-            reason: "Withdraws cannot exceed deposits",
-            isDisabled: true,
-            value: depositedAmount,
-          },
-          {
-            reason: "Insufficient liquidity to borrow",
-            isDisabled: true,
-            value: reserve.availableAmount.minus(
-              new BigNumber(MIN_AVAILABLE_AMOUNT).div(
-                10 ** reserve.mintDecimals,
-              ),
-            ),
-          },
-          {
-            reason: "Withdraw is unhealthy",
-            isDisabled: true,
-            value:
-              !obligation ||
-              obligation.maxPriceWeightedBorrowsUsd.gt(
-                obligation.minPriceBorrowLimitUsd,
-              )
-                ? new BigNumber(0)
-                : obligation.minPriceBorrowLimitUsd
-                    .minus(obligation.maxPriceWeightedBorrowsUsd)
-                    .div(reserve.minPrice)
-                    .div(reserve.config.openLtvPct / 100),
-          },
-          {
-            reason: "Pool outflow rate limit surpassed",
-            isDisabled: true,
-            value: data.lendingMarket.rateLimiter.remainingOutflow.div(
-              reserve.maxPrice,
-            ),
-          },
-        ];
-      })();
-
-      const getMaxValue = () => {
-        return BigNumber.max(
-          new BigNumber(0),
-          BigNumber.min(
-            ...Object.values(maxCalculations).map((calc) => calc.value),
-          ),
-        ).toFixed(reserve.mintDecimals, BigNumber.ROUND_DOWN);
-      };
-
       const getNewCalculations = (value: string) => {
         if (!value.length)
           return {
@@ -426,62 +195,20 @@ export default function ActionsModal() {
         };
       };
 
-      const getSubmitButtonState = (value: string) => {
-        for (const calc of maxCalculations) {
-          if (new BigNumber(value).gt(calc.value))
-            return { isDisabled: calc.isDisabled, title: calc.reason };
-        }
-        return undefined;
-      };
-
       return {
         action: Action.WITHDRAW,
         actionPastTense: "withdrew",
-        getMaxValue,
+        getMaxValue: getMaxValue(Action.WITHDRAW, reserve, data, obligation),
         getNewCalculations,
-        getSubmitButtonState,
+        getSubmitButtonState: getSubmitButtonState(
+          Action.WITHDRAW,
+          reserve,
+          data,
+          obligation,
+        ),
         submit: withdraw,
       };
     } else if (selectedTab === Tab.REPAY) {
-      const maxCalculations = (() => {
-        const coinBalanceForReserve =
-          data.coinBalancesMap[reserve.coinType]?.balance ?? new BigNumber(0);
-        const position = obligation?.borrows.find(
-          (borrow) => borrow.coinType === reserve.coinType,
-        );
-        const borrowedAmount = position?.borrowedAmount ?? new BigNumber(0);
-
-        const result = [
-          {
-            reason: "Insufficient balance",
-            isDisabled: true,
-            value: coinBalanceForReserve,
-          },
-          {
-            reason: "Repay amount exceeds borrowed amount",
-            isDisabled: true,
-            value: borrowedAmount,
-          },
-        ];
-        if (isSui(reserve.coinType))
-          result.push({
-            reason: `${SUI_REPAY_GAS_MIN} SUI should be saved for gas`,
-            isDisabled: true,
-            value: coinBalanceForReserve.minus(SUI_REPAY_GAS_MIN),
-          });
-
-        return result;
-      })();
-
-      const getMaxValue = () => {
-        return BigNumber.max(
-          new BigNumber(0),
-          BigNumber.min(
-            ...Object.values(maxCalculations).map((calc) => calc.value),
-          ),
-        ).toFixed(reserve.mintDecimals, BigNumber.ROUND_DOWN);
-      };
-
       const getNewCalculations = (value: string) => {
         if (!value.length)
           return {
@@ -514,29 +241,25 @@ export default function ActionsModal() {
         };
       };
 
-      const getSubmitButtonState = (value: string) => {
-        for (const calc of maxCalculations) {
-          if (new BigNumber(value).gt(calc.value))
-            return { isDisabled: calc.isDisabled, title: calc.reason };
-        }
-        return undefined;
-      };
-
       return {
         action: Action.REPAY,
         actionPastTense: "repaid",
-        getMaxValue,
+        getMaxValue: getMaxValue(Action.REPAY, reserve, data, obligation),
         getNewCalculations,
-        getSubmitButtonState,
+        getSubmitButtonState: getSubmitButtonState(
+          Action.REPAY,
+          reserve,
+          data,
+          obligation,
+        ),
         submit: repay,
       };
     }
   }, [
     selectedTab,
     reserve,
-    data.coinBalancesMap,
+    data,
     obligation,
-    data.lendingMarket.rateLimiter.remainingOutflow,
     deposit,
     borrow,
     withdraw,
