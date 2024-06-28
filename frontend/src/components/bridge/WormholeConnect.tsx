096c25b490524696596af0e51c886739f0f5a011
0xshiba1 <158560741+0xshiba1@users.noreply.github.com>
2024-06-28 11:27:27 +0800

    add base and arbitrum support

--- frontend/src/components/bridge/WormholeConnect.tsx
+++ frontend/src/components/bridge/WormholeConnect.tsx
@@ -142,23 +142,15 @@ export default function WormholeConnect({ isHidden }: WormholeConnectProps) {
               "https://rpc.ankr.com/eth/d57d49c5cc988185579623ea8fc23e7a0fc7005e843939bc29ed460952b381cb",
             solana: `https://solendf-solendf-67c7.rpcpool.com/${process.env.NEXT_PUBLIC_SOL_TRITON_ONE_DEV_API_KEY ?? ""}`,
             sui: rpc.url,
+            base: "https://rpc.ankr.com/base/d57d49c5cc988185579623ea8fc23e7a0fc7005e843939bc29ed460952b381cb",
+            arbitrum:
+              "https://rpc.ankr.com/arbitrum/d57d49c5cc988185579623ea8fc23e7a0fc7005e843939bc29ed460952b381cb",
           },
-          tokens: [
-            "WETH",
-            "USDCeth",
-            // "USDCpolygon",
-            // "USDCavax",
-            // "USDCsol",
-            // "USDCarbitrum",
-            // "USDCoptimism",
-            // "USDCbase",
-            "USDT",
-            // "SUI",
-          ],
+          tokens: ["WETH", "USDCeth", "USDT"],
           cta: { text: "Deposit", link: DASHBOARD_URL },
           bridgeDefaults: { toNetwork: "sui", requiredNetwork: "sui" },
           pageHeader: "Bridge",
-          networks: ["ethereum", "solana", "sui"],
+          networks: ["ethereum", "solana", "sui", "base", "arbitrum"],
           showHamburgerMenu: false,
         })}
         data-theme={JSON.stringify({
